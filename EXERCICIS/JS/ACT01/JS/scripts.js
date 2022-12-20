9f66e2eb4c5edf4e146f99f3a6087e19f27887bd
Pauuu2149 <121053585+Pauuu2149@users.noreply.github.com>
2022-12-20 10:00:00 +0100

    Add files via upload

---
+++ EXERCICIS/JS/ACT01/JS/scripts.js
@@ -0,0 +1,9 @@
+let a;
+let b;
+let c;
+a = 'hola mundo';
+b = 'siuuu';
+
+document.getElementById("result1").innerHTML = a;
+document.getElementById("result2").innerHTML = b;
+document.getElementById("result3").innerHTML = c;
